934b9a59fcaa630a8655926ab770cc467a3a655b
BK <brian_j_king@me.com>
2020-08-21 12:55:42 -0500

    first commit

---
+++ FunWithVariables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FunWithVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Brian";
+            int age = 43;
+            char nickname = 'B';
+            bool hasBeard = true;
+            double powerLevel = 1000.00;
+            decimal beardRating = 100m;
+
+            Console.WriteLine($"Name:{name} Age:{age} Nickname:{nickname} \n" +
+                $"Beard:{hasBeard} Power Level:{powerLevel} Beard Rating:{beardRating}");
+
+            Console.ReadLine();
+        }
+    }
+}
